ee4cf1a23d59ef69560e6ae0c7b9fd26d81d5bd3
efgcb <efgcb@windowslive.com>
2020-01-13 23:00:41 -0300

    Salvando os dados do usuario logado no localStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { EstadoService } from '../services/donain/estado.service';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { AuthService } from '../services/auth.service';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     EstadoService,
     ErrorInterceptorProvider,
-    AuthService
+    AuthService,
+    StorageService
   ]
 })
 export class AppModule {}
